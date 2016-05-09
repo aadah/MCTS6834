a9a83ca816922df3888d67e9c8d28847f87c18e0
James Hobin <hobinjk@mit.edu>
2016-05-09 11:31:21 -0400

    Improve visualizer

--- snake-visualizer/snakes.js
+++ snake-visualizer/snakes.js
@@ -2,6 +2,7 @@ var gameId, state = {board: [[]]};
 var canvas = document.getElementById('game');
 canvas.width = 640;
 canvas.height = 640 * 25 / 40;
+var cellSize = 640 / 40;
 var gfx = canvas.getContext('2d');
 
 function update() {
@@ -26,23 +27,72 @@ function draw() {
         continue;
       }
 
-      if (token === 'R') {
-        gfx.fillStyle = '#0035FF'
-      }
-      if (token === 'B') {
+      switch (token) {
+      case 'R':
+      case '+':
         gfx.fillStyle = '#FF3900';
-      }
-      if (token === '@') {
+        break;
+      case 'B':
+      case 'x':
+        gfx.fillStyle = '#0035FF'
+        break;
+      case '@':
         gfx.fillStyle = '#68CC00';
+        break;
       }
 
-      var cellWidth = canvas.width / map[row].length;
-      var cellHeight = canvas.height / map.length;
-      var x = col * cellWidth;
-      var y = row * cellHeight;
+      var x = col * cellSize;
+      var y = row * cellSize;
+
+
+      if (token !== '+' && token !== 'x') {
+        gfx.fillRect(x, y, cellSize, cellSize);
+        continue;
+      }
+      var dir = state.rDir;
+      if (token === 'x') {
+        dir = state.bDir;
+      }
+      var cellWidth = cellSize;
+      var cellHeight = cellSize;
+      var centerX = x + cellSize / 2;
+      var centerY = y + cellSize / 2;
+      var startAngle = 0;
+      var endAngle = Math.PI;
+      switch (dir) {
+      case 0:
+        break;
+      case 3:
+        cellWidth /= 2;
+        startAngle = -Math.PI / 2;
+        endAngle = Math.PI / 2;
+        break;
+      case 2:
+        cellHeight /= 2;
+        startAngle = 0;
+        endAngle = Math.PI;
+        break;
+      case 1:
+        x = centerX;
+        cellWidth /= 2;
+        startAngle = Math.PI / 2;
+        endAngle = 3 * Math.PI / 2;
+        break;
+      case 4:
+        y = centerY;
+        cellHeight /= 2;
+        startAngle = Math.PI;
+        endAngle = 2 * Math.PI;
+        break;
+      }
+      gfx.beginPath()
+      gfx.arc(centerX, centerY, cellSize / 2, startAngle, endAngle);
+      gfx.closePath();
+      gfx.fill();
       gfx.fillRect(x, y, cellWidth, cellHeight);
     }
   }
+
   window.requestAnimationFrame(draw);
 }
 
