ad1a1e15f772fa1755e42bb6338be914c89d0e4a
James Hobin <hobinjk@mit.edu>
2016-05-09 20:18:28 -0400

    Snake faces, or snaces

--- snake-visualizer/snakes.js
+++ snake-visualizer/snakes.js
@@ -1,8 +1,8 @@
 var gameId, state = {board: [[]]};
 var canvas = document.getElementById('game');
-canvas.width = 640;
-canvas.height = 640 * 25 / 40;
-var cellSize = 640 / 40;
+var cellSize = 28;
+canvas.width = cellSize * 40;
+canvas.height = cellSize * 25;
 var gfx = canvas.getContext('2d');
 
 function update() {
@@ -57,39 +57,56 @@ function draw() {
       var cellHeight = cellSize;
       var centerX = x + cellSize / 2;
       var centerY = y + cellSize / 2;
-      var startAngle = 0;
-      var endAngle = Math.PI;
+      var dirAngle = 0;
       switch (dir) {
       case 0:
         break;
       case 3:
         cellWidth /= 2;
-        startAngle = -Math.PI / 2;
-        endAngle = Math.PI / 2;
+        dirAngle = 0;
         break;
       case 2:
         cellHeight /= 2;
-        startAngle = 0;
-        endAngle = Math.PI;
+        dirAngle = Math.PI / 2;
         break;
       case 1:
         x = centerX;
         cellWidth /= 2;
-        startAngle = Math.PI / 2;
-        endAngle = 3 * Math.PI / 2;
+        dirAngle = Math.PI;
         break;
       case 4:
         y = centerY;
         cellHeight /= 2;
-        startAngle = Math.PI;
-        endAngle = 2 * Math.PI;
+        dirAngle = 3 * Math.PI / 2;
         break;
       }
+      var startAngle = dirAngle - Math.PI / 2;
+      var endAngle = dirAngle + Math.PI / 2;
       gfx.beginPath()
       gfx.arc(centerX, centerY, cellSize / 2, startAngle, endAngle);
       gfx.closePath();
       gfx.fill();
       gfx.fillRect(x, y, cellWidth, cellHeight);
+
+      gfx.fillStyle = 'white';
+      gfx.translate(centerX, centerY);
+      gfx.rotate(dirAngle);
+
+      gfx.beginPath();
+      gfx.arc(cellSize * 0.2, -cellSize * 0.2, cellSize * 0.17, 0, 2 * Math.PI);
+      gfx.fill();
+      gfx.beginPath();
+      gfx.arc(cellSize * 0.2, cellSize * 0.2, cellSize * 0.17, 0, 2 * Math.PI);
+      gfx.fill();
+
+      gfx.fillStyle = 'black';
+      gfx.beginPath();
+      gfx.arc(0.23 * cellSize, -0.2 * cellSize, cellSize * 0.1, 0, 2 * Math.PI);
+      gfx.fill();
+      gfx.beginPath();
+      gfx.arc(0.23 * cellSize, 0.2 * cellSize, cellSize * 0.1, 0, 2 * Math.PI);
+      gfx.fill();
+      gfx.setTransform(1, 0, 0, 1, 0, 0);
     }
   }
 
